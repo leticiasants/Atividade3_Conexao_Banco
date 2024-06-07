7a8908ce761da310796cd44db6aa6e70d8507bd3
Huandy <huandysilva@gmail.com>
2024-06-07 14:05:02 -0300

    Dao usando driver psycopg

---
+++ Driver/dao.py
@@ -0,0 +1,105 @@
+import psycopg
+from psycopg.errors import OperationalError, DatabaseError
+from model import Order, OrderDetail, Employee, Customer
+
+#Classe com métodos genéricos para execução de queries e processamento dos resultados
+class Auxiliar:
+
+    #método que recebe a query a ser executada e seus parâmetros.  Cria a conexão com o banco
+    def executar_query(self, sql, parametros):
+        try:
+            with psycopg.connect(host='localhost', dbname='northwind', user='postgres', password='root') as northwind:
+                with northwind.cursor() as sessao:
+                    sessao.execute(sql, parametros)
+                    #checa se o comando executado retornou resultados
+                    if sessao.description is not None:
+                        colunas_metadado = [desc[0] for desc in sessao.description]
+                        return colunas_metadado, sessao.fetchall() #retorna uma tuple com os registros e os metadados para facilitar o processamento
+        except OperationalError as e:
+            print(f"Erro de conexão: {e}")
+            return None
+        except DatabaseError as e:
+            print(f"Erro no banco de dados: {e}")
+            return None
+        except Exception as e:
+            print(f"Erro inesperado: {e}")
+            return None
+
+    #método que recebe o conteudo retornado por um select e processa os resultados, de modo a transformá-los em objetos model
+    def processar(self, resultados, nome_classe):
+        lista_resultados = [] #lista que vai conter os objetos model para retorno
+        metadados, registros = resultados #separa os metadados e os registros retornados pela função executar_query
+        for registro in registros:
+            dicionario = dict(zip(metadados, registro)) #cria um dicionário no qual as chaves são o nome da coluna na tabela
+            lista_resultados.append(nome_classe(**dicionario)) #instancia um objeto da classe passada como parâmetro para a função e o coloca na lista de resultados. O dicionário é usado para passar os parâmetros de forma nomeada para o construtor da classe
+        return lista_resultados if len(lista_resultados) > 1 else lista_resultados[0] #retorna a lista de resultados se a consulta retornou mais de um registro. Caso contrário, retorna somente o objeto. (útil para quando um registro é filtrado por id nas classes dao)
+
+
+class OrderDao:
+    def __init__(self) -> None:
+        self.auxiliar = Auxiliar()
+
+    def inserir(self, pedido):
+        sql = "Insert into northwind.orders (orderid, customerid, employeeid, orderdate) values (%s,%s,%s,%s)"
+        parametros = (pedido.orderid, pedido.customerid,
+                      pedido.employeeid, pedido.orderdate)
+        self.auxiliar.executar_query(sql, parametros)
+
+    def obter(self, id=None):
+        sql = 'select * from northwind.orders'
+        parametros = None
+        if id is not None:
+            sql += ' where orderid=%s'
+            parametros = (id,)
+        res = self.auxiliar.executar_query(sql, parametros)
+        return self.auxiliar.processar(res, Order)
+
+
+class OrderDetailsDao:
+    def __init__(self) -> None:
+        self.auxiliar = Auxiliar()
+
+    def inserir(self, detalhes_pedido):
+        sql = "Insert into northwind.order_details (orderid, productid, unitprice, quantity) values (%s,%s,%s,%s)"
+        parametros = (detalhes_pedido.orderid, detalhes_pedido.productid,
+                      detalhes_pedido.unitprice, detalhes_pedido.quantity)
+        self.auxiliar.executar_query(sql, parametros)
+
+    def obter(self, id=None):
+        sql = 'select * from northwind.order_details'
+        parametros = None
+        if id is not None:
+            sql += ' where orderid=%s'
+            parametros = (id,)
+        res = self.auxiliar.executar_query(sql, parametros)
+        return self.auxiliar.processar(res, OrderDetail)
+
+
+class FuncionarioDao:
+    def __init__(self) -> None:
+        self.auxiliar = Auxiliar()
+
+    def obter(self, primeiro_nome=None, ultimo_nome=None):
+        sql = 'select * from northwind.employees'
+        parametros = None
+        if primeiro_nome is not None and ultimo_nome is not None:
+            sql += ' where firstname=%s and lastname=%s'
+            parametros = (primeiro_nome,ultimo_nome)
+        res = self.auxiliar.executar_query(sql, parametros)
+        return self.auxiliar.processar(res, Employee)
+
+
+class ClienteDao:
+    def __init__(self) -> None:
+        self.auxiliar = Auxiliar()
+
+    def obter(self, id=None):
+        sql = 'select * from northwind.customers'
+        parametros = None
+        if id is not None:
+            sql += ' where customerid=%s'
+            parametros = (id,)
+        res = self.auxiliar.executar_query(sql, parametros)
+        return self.auxiliar.processar(res, Customer)
+
+
